443f9a72292949dcd48ed061ee7c065b3ec58938
ChrolloKryber <rishayraghav00@gmail.com>
2024-05-26 16:04:22 +0530

    script done

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import time
+import datetime
+from pynput.keyboard import Key, Controller
+import subprocess
+import random
+
+with open("keywords.txt", "r") as keywords:
+    queries = keywords.readlines()
+
+keyboard = Controller()
+
+
+def search_bar():
+    keyboard.press(Key.ctrl)
+    keyboard.press("l")
+    keyboard.release(Key.ctrl)
+    keyboard.release("l")
+
+
+def press_enter():
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+
+
+def bing_search(word):
+    search_bar()
+    time.sleep(2)
+    keyboard.type(word)
+    time.sleep(2)
+    press_enter()
+
+
+def launch_edge():
+    with open(os.devnull, "w") as devnull:
+        subprocess.Popen(["chromium"], stdout=devnull, stderr=devnull)
+
+
+def close_edge():
+    os.system("pkill -f chromium")
+
+
+
+print(f"Start Time : {datetime.datetime.now().time()}")
+
+launch_edge()
+
+time.sleep(3)
+
+for i in range(35):
+    words = random.choice(queries)
+    print(f"Query {i + 1}: {words}", end="")
+    bing_search(words)
+    time.sleep(12)
+
+close_edge()
+print(f"Kill Time : {datetime.datetime.now().time()}")
